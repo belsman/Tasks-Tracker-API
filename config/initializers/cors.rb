59adfce4c376289dcf4fb664c51781016d2487b7
Bello Babakolo <fleetbeekay@gmail.com>
2021-04-21 03:44:07 +0100

    Fix config.ru

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -13,6 +13,4 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
-
-  Rails.application.config.hosts << "https://time-task-tracker.herokuapp.com/"
 end
