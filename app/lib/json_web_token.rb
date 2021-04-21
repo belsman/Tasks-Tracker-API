e9c57b9e8be5f6ab64cae7727495a89de9c8a60e
Bello Babakolo <fleetbeekay@gmail.com>
2021-04-21 11:27:58 +0100

    Fix the key

--- app/lib/json_web_token.rb
+++ app/lib/json_web_token.rb
@@ -1,23 +1,23 @@
 class JsonWebToken
-  # secret to encode and decode token
-  HMAC_SECRET = Rails.application.secrets.secret_key_base
-  puts "HMAC_SECRET IS"
-  puts HMAC_SECRET
-
+  # secret to encode and decode token  
   def self.encode(payload, exp = 24.hours.from_now)
     # set expiry to 24 hours from creation time
     payload[:exp] = exp.to_i
     # sign token with application secret
-    JWT.encode(payload, HMAC_SECRET)
+    JWT.encode(payload, secret_key)
   end
 
   def self.decode(token)
     # get payload; first index in decoded Array
-    body = JWT.decode(token, HMAC_SECRET)[0]
+    body = JWT.decode(token, secret_key)[0]
     HashWithIndifferentAccess.new body
     # rescue from all decode errors
   rescue JWT::DecodeError => e
     # raise custom error to be handled by custom handler
     raise ExceptionHandler::InvalidToken, e.message
+  end
+
+  def secret_key
+    Rails.application.secrets&.secret_key_base || Rails.application.credentials&.secret_key_base
   end
 end
